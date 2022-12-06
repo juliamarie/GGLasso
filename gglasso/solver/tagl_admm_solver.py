91f404195fe207d821eafd348a6e5d2ef5b348fc
Julia Kilger <julia.kilger@tum.de>
2022-12-06 10:37:33 +0100

    tests

--- gglasso/solver/tagl_admm_solver.py
+++ gglasso/solver/tagl_admm_solver.py
@@ -17,7 +17,7 @@ devtools = importr('devtools')
 taglasso = importr('taglasso')
 
 
-def admm_tagl(S, A, p, T, lam1, lam2, rho, t, Om0, Gam0, Atilde, A_for_gamma, A_for_B, C, C_for_D, stat, eps_abs, eps_rel=1e-3):
+def admm_tagl(S, A, p, T, lam1, lam2, rho, t, Om0, Gam0, A_for_gamma, A_for_B, C, C_for_D, stat, eps_abs, eps_rel=1e-4):
     assert (np.linalg.norm(p) >= 1e-10), "Dimension of the problem should be greater than 0"
     # initialize
     U1 = Om0.copy()
@@ -33,6 +33,7 @@ def admm_tagl(S, A, p, T, lam1, lam2, rho, t, Om0, Gam0, Atilde, A_for_gamma, A_
     Om = Om0.copy() #np.zeros((p, p))  # np.identity(p)#Om0.copy()
     Gam = Gam0.copy() #np.zeros((T, p))  # Gam0.copy()#np.ones((T,p))
     D = np.zeros((p, p))
+    it = 100000
 
     # \tilde A for \Gamma^{(2)} and D
     # Atilde = np.zeros((p + T, T))
@@ -71,7 +72,7 @@ def admm_tagl(S, A, p, T, lam1, lam2, rho, t, Om0, Gam0, Atilde, A_for_gamma, A_
         o = Om - U3 / rho
         for i in range(p):
             Om3[i][i] = o[i][i]
-        print("Omega 3: ", Om3)
+        # print("Omega 3: ", Om3)
 
         # Update \Gamma^{(1)} by groupwise soft-thresholding
         if lam1 == 0:
@@ -124,14 +125,16 @@ def admm_tagl(S, A, p, T, lam1, lam2, rho, t, Om0, Gam0, Atilde, A_for_gamma, A_
         r = tl.r_k(Om1, Om2, Om3, Gam1, Gam2, Om, Gam)
         eps_pri = tl.eps_pri(Om1, Om2, Om3, Gam1, Gam2, Om, Gam, eps_abs, eps_rel)
         eps_dual = tl.eps_dual(U1, U2, U3, U4, U5, eps_abs, eps_rel)
-        print(rho)
+        # print(rho)
         # print(np.linalg.norm(r))
 
-        if (np.linalg.norm(r) <= eps_pri and np.linalg.norm(s) <= eps_dual):
-            stat = 1
-            break
+        if k>=1:
+            if np.linalg.norm(r) <= eps_pri and np.linalg.norm(s) <= eps_dual:
+                stat = 1
+                it = k
+                break
 
-    return Om, Gam, D
+    return Om, Gam, D, stat, it
 
 
 def la_admm_tagl(S, A, p, T, lam1, lam2, rho, t, K, eps_abs, eps_rel=1e-3):
@@ -141,56 +144,59 @@ def la_admm_tagl(S, A, p, T, lam1, lam2, rho, t, K, eps_abs, eps_rel=1e-3):
     Atilde, A_for_gamma, A_for_B, C, C_for_D = tl.A_precompute(A, p, T)
     stat = 0
     for l in range(K):
-        Om, Gam, D = admm_tagl(S, A, p, T, lam1, lam2, rho, t, Om, Gam, Atilde, A_for_gamma, A_for_B, C, C_for_D, stat, eps_abs, eps_rel)
+        Om, Gam, D, stat, it = admm_tagl(S, A, p, T, lam1, lam2, rho, t, Om, Gam, A_for_gamma, A_for_B, C, C_for_D, stat, eps_abs, eps_rel)
 
         # Update penalty parameter
         rho = 2 * rho
         if stat == 1:
-            print("ended in iteration: ", l)
+            print("ended in iteration: ", l, it)
             break
     return Om, Gam, D
 
 
-p = 4
-T = p + 4
-rho = 1
-lam1 = 1
-lam2 = 4
-t = 100
-K = 10
-
-M = np.random.rand(p, p)
-U = np.tril(M)
-S = np.matmul(U, np.transpose(U))
-sdf = pd.DataFrame(S)
-
-with localconverter(robjects.default_converter + pandas2ri.converter):
-    r_s = robjects.conversion.py2rpy(sdf)
-r_vec = robjects.FloatVector([1.0, 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1.])
-r_a = robjects.r['matrix'](r_vec, nrow=4)
-
-A = np.zeros((p, T))
-A[:, :p] = np.identity(p)
-A[:, p + 3] = np.ones(p)
-A[0][p] = 1
-A[1][p + 1] = 1
-for k in range(2, p):
-    A[k][p + 2] = 1
-
-adf = pd.DataFrame(A)
+# p = 4
+# T = p + 4
+# rho = 1
+# lam1 = 1000
+# lam2 = 1000
+# t = 100
+# K = 10
+#
+# M = np.random.rand(p, p)
+# U = np.tril(M)
+# S = np.matmul(U, np.transpose(U))
+# sdf = pd.DataFrame(S)
+#
 # with localconverter(robjects.default_converter + pandas2ri.converter):
-#     r_a = robjects.conversion.py2rpy(adf)
-
-
-tag_lasso = robjects.r['taglasso']
-
-print("tag-lasso")
-print(la_admm_tagl(S, np.identity(p), p, p, lam1, lam2, rho, t, K, 1e0))
-# print(la_admm_tagl(S, A, p, T, lam1, lam2, rho, t, K))
-
-print("i dont care")
-
-print(tag_lasso(X=r_s, A=r_a, lambda1=lam1, lambda2=lam2, rho=rho))
-
-# print("glasso")
-# ADMM_SGL(S, lam2, np.zeros((p,p)))
+#     r_s = robjects.conversion.py2rpy(sdf)
+# r_vec = robjects.FloatVector([1.0, 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1.])
+# r_a = robjects.r['matrix'](r_vec, nrow=4)
+#
+# A = np.zeros((p, T))
+# A[:, :p] = np.identity(p)
+# A[:, p + 3] = np.ones(p)
+# A[0][p] = 1
+# A[1][p + 1] = 1
+# for k in range(2, p):
+#     A[k][p + 2] = 1
+#
+# adf = pd.DataFrame(A)
+# # with localconverter(robjects.default_converter + pandas2ri.converter):
+# #     r_a = robjects.conversion.py2rpy(adf)
+#
+#
+# tag_lasso = robjects.r['taglasso']
+#
+# print("tag-lasso")
+# #print(la_admm_tagl(S, np.identity(p), p, p, lam1, lam2, rho, t, K, 1e-19, 1e-4))
+# print(la_admm_tagl(S, A, p, T, lam1, lam2, rho, t, K, 1e-19))
+#
+# print("................")
+#
+# print(np.linalg.inv(S))
+#
+#
+# #print(tag_lasso(X=r_s, A=r_a, lambda1=lam1, lambda2=lam2, rho=rho))
+#
+# # print("glasso")
+# # ADMM_SGL(S, lam2, np.zeros((p,p)))
---
+++ gglasso/tests/test_func_tagl.py
@@ -0,0 +1,75 @@
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+
+from gglasso.solver.tagl_admm_solver import la_admm_tagl
+from gglasso.solver.single_admm_solver import ADMM_SGL
+
+
+def test_example_tree_big_lambda2():
+    A = np.zeros((5, 8))
+    A[:5, :5] = np.identity(5)
+    v = [1, 1, 1, 0, 0]
+    w = [0, 0, 0, 1, 1]
+    A[:, 5] = v
+    A[:, 6] = w
+    A[:, 7] = np.ones(5)
+    W = np.random.rand(2)
+    V = np.random.rand(3)
+    S = np.zeros((5, 5))
+    S[:3, :3] = W @ W.T
+    S[3:, 3:] = V @ V.T
+    Omega, Gamma, D = la_admm_tagl(S, A, 5, 8, 1, 100, 1, 100, 10, 1e-5, 1e-5)
+    for i in range(5):
+        Omega[i][i] = 1
+    assert_array_almost_equal(Omega, np.identity(5), 5)
+    return
+
+
+def test_example_tree_big_lambda1():
+    A = np.zeros((5, 8))
+    A[:5, :5] = np.identity(5)
+    v = [1, 1, 1, 0, 0]
+    w = [0, 0, 0, 1, 1]
+    A[:, 5] = v
+    A[:, 6] = w
+    A[:, 7] = np.ones(5)
+    W = np.random.rand(2)
+    V = np.random.rand(3)
+    S = np.zeros((5, 5))
+    S[:3, :3] = W @ W.T
+    S[3:, 3:] = V @ V.T
+    Omega, Gamma, D = la_admm_tagl(S, A, 5, 8, 100, 1, 1, 100, 10, 1e-5, 1e-5)
+    Omega2 = np.zeros((5, 5))
+    Omega2.fill(Gamma[1][1])
+    for i in range(5):
+        Omega[i][i] = Gamma[1][1]
+    assert_array_almost_equal(Omega, Omega2, 5)
+    return
+
+
+def test_example_tree_glasso():
+    A = np.zeros((5, 8))
+    A[:5, :5] = np.identity(5)
+    v = [1, 1, 1, 0, 0]
+    w = [0, 0, 0, 1, 1]
+    A[:, 5] = v
+    A[:, 6] = w
+    A[:, 7] = np.ones(5)
+    W = np.random.rand(2)
+    V = np.random.rand(3)
+    S = np.zeros((5, 5))
+    S[:3, :3] = W @ W.T
+    S[3:, 3:] = V @ V.T
+    Omega, Gamma, D = la_admm_tagl(S, A, 5, 8, 1e-7, 1, 1, 100, 10, 1e-7, 1e-4)
+    sol, info = ADMM_SGL(S, 1, np.zeros((5, 5)))
+    Omega2 = sol.get('Omega')
+    for i in range(5):
+        Omega[i][i] = 1
+        Omega2[i][i] = 1
+    assert_array_almost_equal(Omega, Omega2, 5)
+    return
+
+
+test_example_tree_big_lambda2()
+test_example_tree_big_lambda1()
+test_example_tree_glasso()
